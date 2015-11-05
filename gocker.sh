b5940b7fd256e18d5ce25fc6fe71c9609c08a282
robmadeyou <robmadeyou@gmail.com>
2015-11-05 15:18:16 +0000

    Added a throwaway command and status

--- gocker.sh
+++ gocker.sh
@@ -27,6 +27,10 @@ function main {
         execute "-it" "${@:2}" "/bin/bash";
     elif [ "$1" == "run" ]; then
         execute "${@:2}";
+    elif [ "$1" == "status" ]; then
+        status;
+    elif [[ "$1" == "throw" ]] || [[ "$1" == "throwaway" ]] || [[ "$1" == "th" ]]; then
+        throwaway "${@:2}";
     else
         echo "Not a valid command, refer to the help menu:";
         printHelp;
@@ -47,6 +51,35 @@ function execute {
 
 function printHelp {
     echo "I'm helping!";
+}
+
+function status {
+up=`docker ps -q | wc -l`;
+total=`docker ps -aq | wc -l`;
+    docker ps;
+    if [ $up -ne $total ]; then
+        echo "$up/$total containers are running."
+        echo "Would you like to [S]tart the non-running ones, [R]emove them, or [I]gnore this?";
+        read in;
+        if [[ "$in" == "s" ]] || [[ "$in" == "S" ]]; then
+            echo 'Starting containers';
+            docker start $(docker ps -qf status=exited);
+            echo 'Done.';
+        elif [[ "$in" == "r" ]] || [[ "$in" == "R" ]]; then
+            echo 'Removing containers...';
+            docker rm -f $(docker ps -qf status=exited);
+            echo 'Done.';
+        else
+            echo 'Ignoring...';
+        fi
+    fi
+}
+
+function throwaway {
+    if [ "$#" -lt 1 ]; then
+        echo 'This function needs a command to run';
+    fi
+    docker run --rm -it ubuntu $1
 }
 
 main "$@";
